e8a083fd11162193a415cf3aaf3f6f8f3ebddf04
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-26 20:26:34 -0800

    add: timerライブラリ追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
+const changeHourMinSec = (remainingTime: number) => {
+  // const hours = Math.floor(remainingTime / 3600)
+  const minutes = Math.floor((remainingTime % 3600) / 60)
+  const seconds = remainingTime % 60
+
+  return `${minutes}:${seconds}`
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <div className="text-green-600">fjdkslajfl</div>
+    <h1 className="text-blue-600 flex justify-center py-24 text-5xl">
+      Hello React Pomodoro Timer with BayAreaHackers :)
+    </h1>
+    <div className="flex justify-center">
+      <CountdownCircleTimer
+        isPlaying
+        duration={1500}
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+      >
+        {({ remainingTime }) => changeHourMinSec(remainingTime)}
+      </CountdownCircleTimer>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
