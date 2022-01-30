8ec98677a0416f812c77ebb9f2ea3e539d659a53
Sota1221 <skobayashi009@berkeley.edu>
2022-01-29 19:27:56 -0800

    added range func

--- src/components/atoms/SelectBox.tsx
+++ src/components/atoms/SelectBox.tsx
@@ -1,6 +1,3 @@
-// React = require('react');
-// React.Bootstrap = require('react-bootstrap');
-// React.Bootstrap.Select = require('react-bootstrap-select');
 import Form from 'react-bootstrap/Form'
 
 type BoxItem = {
@@ -9,23 +6,28 @@ type BoxItem = {
   initNum: number;
 }
 
+// returns an iterable of concective integers
+const range = (start:number, end:number) => {
+  return Array.from(Array(end - start + 1).keys()).map(x => x + start);
+}
+
 export const SelectBox = (props: BoxItem) => {
   const { isTimer, initNum } = props;
+
+  let nums: number[];
+
+  if (isTimer) {
+    nums = range(1, 60);
+  } else {
+    nums = range(1, 10);
+  };
+
   return (
     <>
       <p>Pomos in each round</p>
       <Form.Select aria-label="Default select example">
         <option>select here</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
+        {nums.map((num: number) => <option value={num.toString()}>{num}</option>)}
       </Form.Select>
     </>
   );
