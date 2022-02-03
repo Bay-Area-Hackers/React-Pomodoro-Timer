be1636882aefeff38db2b24b8ec3636779c4e5c2
Sota1221 <skobayashi009@berkeley.edu>
2022-02-03 12:54:48 -0800

    deleted string(e.g. minutes) in option

--- src/components/atoms/SelectBox.tsx
+++ src/components/atoms/SelectBox.tsx
@@ -5,36 +5,25 @@ import Select from "react-select";
 type Props = {
   isTimer: boolean
   initNum: number
-  setter: (num: number) => void
+  setter: (num) => void
 };
 
 export const SelectBox: React.FC<Props> = ({ isTimer, initNum, setter }) => {
-  const handleValueChange = (event) => {
-    const inputValue = event.target.value
-    setter(inputValue);
-  };
-
   const options: any[] = [];
   const upper: number = isTimer ? 61 : 11;
+
   for(let i = 1; i < upper; i++) {
-    let obj = {};
-    obj['value'] = i;
-    if (i == 1) {
-      obj['label'] = isTimer ? `${i} minute` : `${i} pomo`;
-    } else {
-      obj['label'] = isTimer ? `${i} minutes` : `${i} pomos`;
-    }
-    options.push(obj);
+    options.push({'label': i.toString(), 'value': i});
   };
 
    return (
     <>
       <Select
-        className="w-40"
+        className="w-32"
         maxMenuHeight={250}
         options={options}
         defaultValue={initNum}
-        onChange={handleValueChange}
+        onChange={(value) => setter(value)}
         id="search-select"
       />
     </>
